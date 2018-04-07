6b50c5bfaccbd7419573e3e7fca4da404c5d4b65
KINGGHOST <kingghostsh@gmail.com>
2018-04-07 15:03:57 +0500

    Added Tables

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -13,7 +13,8 @@ import scala.concurrent.duration.DurationInt
 
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents,
-                               indexTemplate: index)
+                               indexTemplate: index,
+                               tableTemplate: table)
                               (implicit val ec: ExecutionContext)
   extends BaseController with LazyLogging {
 
@@ -21,6 +22,9 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
 
   def index = Action {
     Ok(indexTemplate())
+  }
+  def table = Action {
+    Ok(tableTemplate())
   }
 
 
